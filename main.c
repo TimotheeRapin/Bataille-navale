e6dcaf132a8ca9b59bb8e4e6e828fd5e7633b541
RAPIN Timothee <Timothee.RAPIN@cpnv.ch>
2020-03-04 11:58:13 +0100

    Menu

--- main.c
+++ main.c
@@ -6,13 +6,29 @@
 *       Version :                   0.1                                 *
 *       Développer par :            Timothée Rapin                      *
 *       Date de création :          04.03.2020                          *
-*       Date de mise à jour :                                           *
-*       Nouveautés :                                                    *
+*       Date de mise à jour :       04.03.2020                          *
+*       Nouveautés :                Menu                                *
 *                                                                       *
 *************************************************************************
 */
 
 #include <stdio.h>
+
+int fonctionMenu (){
+    int choixMenu = 0;
+
+    printf("\n\n\nMENU :");
+    printf("\n\n1) Afficher l'aide");
+    printf("\n2) S'authentifier");
+    printf("\n3) Choisir une map");
+    printf("\n4) Jouer");
+    printf("\n5) Afficher les scores");
+    printf("\n6) Quiter\n");
+
+    scanf("%d", &choixMenu);
+
+    return choixMenu;
+}
 
 int main() {
     setbuf(stdout,0);
@@ -20,6 +36,60 @@ int main() {
 
     // Déclarations + initialisations
 
+    int menu = 0;
+
+
+    // Titre
+
+    printf("\n\nBATAILLE-NAVALE");
+
+
+    do {
+
+        // MENU
+
+        menu = fonctionMenu();
+
+
+        // AIDE
+
+        if (menu == 1) {
+            printf("\n\nAIDE :");
+        }
+
+
+        // AUTHENTIFICATION
+
+        if (menu == 2) {
+            printf("\n\nAUTHENTIFICATION :");
+        }
+
+
+        // MAPS
+
+        if (menu == 3){
+            printf("\n\nMAPS :");
+            printf("\n\n1) Choisir une map");
+            printf("\n2) Creer une map");
+            printf("\n3) Supprimer une map\n");
+        }
+
+
+        // JEU
+
+        if (menu == 4){
+            printf("\n\nJEU :");
+        }
+
+
+        // SCORES
+
+        if(menu == 5){
+            printf("\n\nSCORES :");
+        }
+
+
+    }while(menu != 6);
 
 
     // Fin
