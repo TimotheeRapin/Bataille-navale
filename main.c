5cefed850d334877354405c2f514d4dbcfefc8d4
RAPIN Timothee <Timothee.RAPIN@cpnv.ch>
2020-03-04 11:24:52 +0100

    Entete

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
+/*
+*************************************************************************
+*                                                                       *
+*       N° de projet :              Pr01                                *
+*       Nom du projet :             Bataille-navale                     *
+*       Version :                   0.1                                 *
+*       Développer par :            Timothée Rapin                      *
+*       Date de création :          04.03.2020                          *
+*       Date de mise à jour :                                           *
+*       Nouveautés :                                                    *
+*                                                                       *
+*************************************************************************
+*/
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    setbuf(stdout,0);
+
+
+    // Déclarations + initialisations
+
+
+
+    // Fin
+
+    printf("\n\n");
     return 0;
 }
