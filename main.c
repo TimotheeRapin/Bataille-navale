bf7b6dd0ba893533b08d1994246f6bdac254d5bb
RAPIN Timothee <Timothee.RAPIN@cpnv.ch>
2020-03-05 16:52:50 +0100

    Affichage grille

--- main.c
+++ main.c
@@ -6,13 +6,20 @@
 *       Version :                   0.1                                 *
 *       Développer par :            Timothée Rapin                      *
 *       Date de création :          04.03.2020                          *
-*       Date de mise à jour :       04.03.2020                          *
-*       Nouveautés :                Bordure titre                       *
+*       Date de mise à jour :       05.03.2020                          *
+*       Nouveautés :                Affichage grille                    *
 *                                                                       *
 *************************************************************************
 */
 
 #include <stdio.h>
+
+// tableau
+
+int col = 0;
+int ligne = 0;
+int tableau[10][10];
+
 
 int fonctionMenu (){
     int choixMenu = 0;
@@ -30,6 +37,74 @@ int fonctionMenu (){
     return choixMenu;
 }
 
+int fonctionGrille(){
+
+    int iFonction = 0;
+    int jFonction = 0;
+    char lettre = 65;
+
+
+    // Affichage numéro de collones
+
+    printf("\n\n     ");
+    for(iFonction = 1; iFonction <= 10; iFonction++){
+        printf("%-6d", iFonction);
+    }
+
+
+    // Affichage bordure haute
+
+    printf("\n  %c", 201);
+    for(iFonction = 1; iFonction < 10; iFonction++){
+        printf("%c%c%c%c%c%c",205,205,205,205,205,203);
+    }
+    printf("%c%c%c%c%c%c",205,205,205,205,205,187);
+
+
+    // Affichage tableau
+
+    for(iFonction = 0; iFonction < 9; iFonction++){
+        printf("\n%c ",lettre);
+        lettre++;
+        for(jFonction = 0; jFonction < 10; jFonction++){
+            printf("%c  %d  ",186,tableau[iFonction][jFonction]);
+        }
+        printf("%c",186);
+
+
+        // Ligne intermediaire
+
+        printf("\n  %c",204);
+        for(jFonction = 1; jFonction < 10; jFonction++){
+            printf("%c%c%c%c%c%c",205,205,205,205,205,206);
+        }
+        printf("%c%c%c%c%c%c",205,205,205,205,205,185);
+    }
+
+
+    // Affichage dernière ligne
+
+    iFonction = 9;
+    printf("\n%c ",lettre);
+    lettre++;
+    for(jFonction = 0; jFonction < 10; jFonction++){
+        printf("%c  %d  ",186,tableau[iFonction][jFonction]);
+    }
+    printf("%c",186);
+
+
+    // dernière bordure
+
+    printf("\n  %c",200);
+    for(jFonction = 1; jFonction < 10; jFonction++){
+        printf("%c%c%c%c%c%c",205,205,205,205,205,202);
+    }
+    printf("%c%c%c%c%c%c",205,205,205,205,205,188);
+
+
+    return 0;
+}
+
 int main() {
     setbuf(stdout,0);
 
@@ -38,6 +113,14 @@ int main() {
 
     int menu = 0;
     int i = 0;
+    int grille;
+
+    for(ligne = 0; ligne < 10; ligne++){
+        for(col = 0; col < 10; col++){
+            tableau[ligne][col] = 0;
+        }
+    }
+
 
 
     // Titre
@@ -67,6 +150,8 @@ int main() {
         if (menu == 1) {
             printf("\n\nAIDE :");
         }
+
+        grille = fonctionGrille();
 
 
         // AUTHENTIFICATION
