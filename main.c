9dd50e52596fa3ad9309e195fedfa1dd823fc7e7
RAPIN Timothee <Timothee.RAPIN@cpnv.ch>
2020-03-06 15:04:13 +0100

    Aide

--- main.c
+++ main.c
@@ -13,12 +13,14 @@
 */
 
 #include <stdio.h>
+#include <windows.h>
 
 // tableau
 
 int col = 0;
 int ligne = 0;
 int tableau[10][10];
+int map;
 
 
 int fonctionMenu (){
@@ -67,7 +69,15 @@ int fonctionGrille(){
         printf("\n%c ",lettre);
         lettre++;
         for(jFonction = 0; jFonction < 10; jFonction++){
-            printf("%c  %d  ",186,tableau[iFonction][jFonction]);
+            switch(tableau[iFonction][jFonction]){
+                case 0: printf("%c     ",186);
+                    break;
+                case 1: printf("%c  %c  ",186,250);
+                    break;
+                case 2: printf("%c  x  ",186);
+                    break;
+                case 3: printf("%c %c%c%c ",186,219,219,219);
+            }
         }
         printf("%c",186);
 
@@ -88,7 +98,15 @@ int fonctionGrille(){
     printf("\n%c ",lettre);
     lettre++;
     for(jFonction = 0; jFonction < 10; jFonction++){
-        printf("%c  %d  ",186,tableau[iFonction][jFonction]);
+        switch(tableau[iFonction][jFonction]){
+            case 0: printf("%c     ",186);
+                break;
+            case 1: printf("%c  %c  ",186,250);
+                break;
+            case 2: printf("%c  x  ",186);
+                break;
+            case 3: printf("%c %c%c%c ",186,219,219,219);
+        }
     }
     printf("%c",186);
 
@@ -105,6 +123,81 @@ int fonctionGrille(){
     return 0;
 }
 
+int fonctionMap(){
+
+    // remise de la map a 0
+
+    for(ligne = 0; ligne < 10; ligne++){
+        for(col = 0; col < 10; col++){
+            tableau[ligne][col] = 0;
+        }
+    }
+
+
+    switch(map){
+
+        // map symboles
+        case 1:
+            tableau[0][0] = 0;
+            tableau[0][1] = 1;
+            tableau[0][2] = 2;
+            tableau[0][3] = 3;
+            break;
+
+            // map bateaux
+        case 2:
+            tableau[0][0] = 3;
+            tableau[0][1] = 3;
+            tableau[0][2] = 3;
+            tableau[0][3] = 3;
+            tableau[0][4] = 3;
+
+            tableau[2][0] = 3;
+            tableau[2][1] = 3;
+            tableau[2][2] = 3;
+            tableau[2][3] = 3;
+
+            tableau[4][0] = 3;
+            tableau[4][1] = 3;
+            tableau[4][2] = 3;
+
+            tableau[6][0] = 3;
+            tableau[6][1] = 3;
+            tableau[6][2] = 3;
+
+            tableau[8][0] = 3;
+            tableau[8][1] = 3;
+            break;
+
+            // map jeux 1
+        case 3:
+            tableau[0][0] = 3;
+            tableau[0][1] = 3;
+            tableau[0][2] = 3;
+            tableau[0][3] = 3;
+            tableau[0][4] = 3;
+
+            tableau[2][0] = 3;
+            tableau[2][1] = 3;
+            tableau[2][2] = 3;
+            tableau[2][3] = 3;
+
+            tableau[4][0] = 3;
+            tableau[4][1] = 3;
+            tableau[4][2] = 3;
+
+            tableau[6][0] = 3;
+            tableau[6][1] = 3;
+            tableau[6][2] = 3;
+
+            tableau[8][0] = 3;
+            tableau[8][1] = 3;
+            break;
+    }
+
+    return 0;
+}
+
 int main() {
     setbuf(stdout,0);
 
@@ -114,6 +207,7 @@ int main() {
     int menu = 0;
     int i = 0;
     int grille;
+    int mapTemp = 0;
 
     for(ligne = 0; ligne < 10; ligne++){
         for(col = 0; col < 10; col++){
@@ -149,9 +243,35 @@ int main() {
 
         if (menu == 1) {
             printf("\n\nAIDE :");
-        }
+            printf("\n\nquand une case n'est pas designee la case reste vide");
+            printf("\nQuand c'est dans l'eau, il y a un %c",250);
+            printf("\nQuand un bateau est touche, il y a un x");
+            printf("\nQuand un bateau est coule, il y a un %c%c%c",219,219,219);
+            printf("\nExemple :");
 
-        grille = fonctionGrille();
+
+            // map exemple
+
+            map = 1;
+            mapTemp = fonctionMap();
+            grille = fonctionGrille();
+
+            printf("\n");
+            system("pause");
+
+            printf("\n\nLes bateaux peuvent etres place verticalement ou horizontalement");
+            printf("\nBateaux disponible :");
+
+
+            // map aide
+
+            map = 2;
+            mapTemp = fonctionMap();
+            grille = fonctionGrille();
+
+            printf("\n");
+            system("pause");
+        }
 
 
         // AUTHENTIFICATION
